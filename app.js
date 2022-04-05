c41cf2a005d38c4986d34fd486726ce06ee79368
Deepeksh <deepeksh77673@gmail.com>
2022-04-05 12:10:04 +0530

    changed local host

--- app.js
+++ app.js
@@ -141,5 +141,5 @@ app.post('/publish', async(req, res)=>{
 
 });
 
-app.listen(3000, () => console.log("server is running on port 3000"))
+app.listen(process.env.PORT || 3000, () => console.log("server is running on port 3000"))
 
