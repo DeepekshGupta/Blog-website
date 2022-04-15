dc5f25b3d80bf9dc277987e37af867c0f93792da
Deepeksh <deepeksh77673@gmail.com>
2022-04-15 17:00:46 +0530

    added images, date and multiple paras

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const {google} = require('googleapis');
 const app = express();
 const _ = require("lodash");
+const date = require(__dirname + "/date.js");
 
 // app.use(express.json())
 app.use(express.urlencoded({
@@ -19,7 +20,7 @@ app.set('view engine', 'ejs');
 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-// let posts1;
+let posts1 = [];
 const id = process.env.SHEET_ID
 let topic = [];
 let Topics = [];
@@ -57,7 +58,9 @@ app.get("/delete", function(req,res){
 });
 
 app.get("/posts/:postName", function(req, res){
-
+// console.log("");
+// console.log("Posts route");
+//     console.log(posts1);
     const requestedTitle = _.lowerCase(req.params.postName);
   
       for(i = 0; i<posts1.values.length;i++){
@@ -65,7 +68,9 @@ app.get("/posts/:postName", function(req, res){
           if (storedTitle === requestedTitle) {
               res.render("post", {
               title: posts1.values[i][1],
-              content: posts1.values[i][2]
+              img: posts1.values[i][3],
+              content: posts1.values[i],
+              date: posts1.values[i][4]
               });
           }
       }
@@ -78,7 +83,7 @@ app.get("/Topics/:postName", function(req, res){
 
     const requestedTag = _.lowerCase(req.params.postName);
       for(i = 0; i<posts1.values.length;i++){ 
-          const storedTag = _.lowerCase(posts1.values[i][3]);
+          const storedTag = _.lowerCase(posts1.values[i][2]);
           if (storedTag === requestedTag) {
             topic.push(posts1.values[i])
           }
@@ -141,11 +146,16 @@ app.get('/', async(req, res)=>{
 app.post('/publish', async(req, res)=>{
 
     try{
-        // console.log(posts1);
-        const ID = posts1.values.length;  
-        const newName  = req.body.Title;
-        const newValue = req.body.Body;
+
+        const title  = req.body.Title;
         const tag = req.body.tag; 
+        const img = req.body.img; 
+        const postdate = date.getDate();
+        const content1 = req.body.Body1;
+        const content2 = req.body.Body2;
+        const content3 = req.body.Body3;
+        const content4 = req.body.Body4;
+
 
         
 
@@ -154,11 +164,11 @@ app.post('/publish', async(req, res)=>{
 
         const writeReq = await sheets.spreadsheets.values.append({
             spreadsheetId: id,
-            range: 'Sheet1', 
+            range: 'Sheet1!A2:AA1000', 
             valueInputOption: 'USER_ENTERED',
             resource: {
                 values: [
-                    [ID, newName, newValue, tag],
+                    [ , title, tag, img, postdate, content1, content2, content3, content4],
                 ]
             }
         })
